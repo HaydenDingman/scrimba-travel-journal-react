57effaeb7e413b5e6a1ed39687f80dc5331f7d21
Hayden Dingman <haydendingman@gmail.com>
2025-07-09 12:14:01 -0700

    Add Entry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Entry from './components/Entry'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+      <Entry />
     </>
   )
 }
---
+++ src/components/Entry.jsx
@@ -0,0 +1,7 @@
+function Entry() {
+    return (
+        <img className="entry-image" src="https://scrimba.com/links/travel-journal-japan-image-url" />
+    )
+}
+
+export default Entry
