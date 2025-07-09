d71687a277fc7da3f0a4505be220c62d4e2893e7
Hayden Dingman <haydendingman@gmail.com>
2025-07-09 12:47:55 -0700

    Use img instead of background image on cards

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,7 +1,9 @@
 function Entry() {
     return (
         <section className="entry-card">
-            <div className="entry-photo"></div>
+            <div className="entry-image-container">
+                <img className="entry-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount Fuji"></img>
+            </div>
             <div className="entry-info">
                 <div className="entry-topbar">
                     <svg className="marker" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">{/*<!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->*/}<path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
